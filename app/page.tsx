9ecf7c14ada9d841f897c280a17ea673703293be
Madhav-Sarma <a.madhavsarma@gmail.com>
2025-07-01 23:04:15 +0530

    basic clerk auth completed

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>lets build ai productivity app</h1>
+      <Button>click me</Button>
+      </main>
+  );
+}
