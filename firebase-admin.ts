54e810a22ff80c7da438e65c3af83d928d4c8a42
Madhav-Sarma <a.madhavsarma@gmail.com>
2025-07-10 16:26:06 +0530

    for deployment

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -2,7 +2,10 @@ import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 
 import { getFirestore } from "firebase-admin/firestore";
 
-const servicekey = require("./service_key.json");
+{/*const servicekey = require("./service_key.json");*/}
+const servicekey = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64!, "base64").toString("utf-8")
+);
 let app: App;
 
 if (getApps().length === 0) {
